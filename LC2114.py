50ef2c2eeb0769eecb3837153260712de3115e9f
Yusuf Hasan <62721671+yh-Yusuf@users.noreply.github.com>
2022-08-17 16:53:44 +0530

    Update LC2114.py

--- LC2114.py
+++ LC2114.py
@@ -1,7 +1,6 @@
 class Solution:
     def mostWordsFound(self, sentences: List[str]) -> int:
-        
-        
+       
         out = 0
         for i in range(len(sentences)):
                if len(sentences[i].split()) > out:
